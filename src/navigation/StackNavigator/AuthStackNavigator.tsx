64311a9145ae69451cfd6a751dc8595e4c5eef26
Cypher Moon <temperaturecaleb@gmail.com>
2024-03-04 19:30:27 +0000

    Add functionality to remember if the user has logged in

--- src/navigation/StackNavigator/AuthStackNavigator.tsx
+++ src/navigation/StackNavigator/AuthStackNavigator.tsx
@@ -1,15 +1,49 @@
-import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import { useIsFocused } from "@react-navigation/native"
+import { NativeStackScreenProps, createNativeStackNavigator } from "@react-navigation/native-stack"
 import LoginScreen from "@screens/Auth/LoginScreen"
 import RegisterScreen from "@screens/Auth/RegisterScreen"
 import OnBoardingScreen from "@screens/OnBoardingScreen"
-import { AuthStackParamList } from "type"
+import SplashScreen from "@screens/SplashScreen"
+import { useEffect, useState } from "react"
+import { useAuthStore } from "store/authStore"
+import { AuthStackParamList, RootStackParamList } from "type"
 
 
 const AuthStack = createNativeStackNavigator<AuthStackParamList>()
+type Props = NativeStackScreenProps<RootStackParamList, "AuthNavigator">
 
-const AuthStackNavigator = () => {
+
+const AuthStackNavigator = ({ navigation }: Props) => {
+    const user = useAuthStore(state => state.user)
+    const isFocused = useIsFocused()
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        if (!isFocused) return
+
+        // navigate to home if user exists
+        if (user) {
+            navigation.navigate('HomeNavigator');
+        }
+
+        // set loading to false after 2 seconds
+        const timeoutRef = setTimeout(() => {
+            setLoading(false)
+        }, 2000)
+
+        // clear timeout
+        return () => clearTimeout(timeoutRef)
+
+    }, [user]);
+
+
+
+    if (loading) {
+        return <SplashScreen />
+    }
+
     return (
-        <AuthStack.Navigator initialRouteName="onBoarding" screenOptions={{ headerShown: false }}>
+        <AuthStack.Navigator screenOptions={{ headerShown: false }}>
             <AuthStack.Screen name="onBoarding" component={OnBoardingScreen} />
             <AuthStack.Screen name="Auth" component={RegisterScreen} />
             <AuthStack.Screen name="Login" component={LoginScreen} />
--- src/navigation/screens/SplashScreen.tsx
+++ src/navigation/screens/SplashScreen.tsx
@@ -1,21 +1,10 @@
-import { NativeStackScreenProps } from '@react-navigation/native-stack'
-import React, { useEffect } from 'react'
+import React from 'react'
 import { SafeAreaView } from 'react-native'
-import { RootStackParamList } from '../../../App'
 import Logo from '../../components/common/Logo'
 import Text from '../../components/common/Text'
 
-type Props = NativeStackScreenProps<RootStackParamList, 'Splash'>
 
-const SplashScreen = ({ navigation }: Props) => {
-
-    // Navigate to onBoarding screen after 3 seconds
-    useEffect(() => {
-        setTimeout(() => {
-            navigation.navigate('onBoarding')
-        }, 3000)
-    }, [])
-
+const SplashScreen = () => {
     return (
         <SafeAreaView className='bg-primary h-screen w-screen flex flex-col justify-center items-center space-y-4'>
             <Logo size='lg' />
