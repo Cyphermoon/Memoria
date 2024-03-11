be2c8b936a15ea3b9f7bf2f766b80fd00c8bffde
Cypher Moon <temperaturecaleb@gmail.com>
2024-03-11 23:29:33 +0000

    Created interface to show liked folder

--- src/navigation/screens/HomeDrawerScreens/ActiveCollectionScreen.tsx
+++ src/navigation/screens/HomeDrawerScreens/ActiveCollectionScreen.tsx
@@ -8,6 +8,9 @@ import { useAuthStore } from 'store/authStore'
 import { CustomCommunityFolderProps, FolderProps } from '@components/Home/type'
 import { doc, onSnapshot } from 'firebase/firestore'
 import { firestoreDB } from 'firebaseConfig'
+import { useIsFocused } from '@react-navigation/native'
+
+//TODO: Add interaction functionalities to this screen (edit, delete, like, unlike, etc)
 
 const ActiveCollectionScreen = () => {
     const insets = useSafeAreaInsets()
@@ -15,9 +18,11 @@ const ActiveCollectionScreen = () => {
 
     const userId = useAuthStore(state => state.user?.uid)
     const activeFolder = useActiveFolderId(userId)
+    const isFocused = useIsFocused()
     const [likedFolder, setLikedFolder] = useState<FolderProps | CustomCommunityFolderProps | null>()
 
     useEffect(() => {
+        if (!isFocused) return
         let folderRef;
         if (activeFolder?.folderCategory === 'personal' && userId && activeFolder?.folderId) {
             folderRef = doc(firestoreDB, 'users', userId, 'folders', activeFolder.folderId);
@@ -39,12 +44,7 @@ const ActiveCollectionScreen = () => {
                 unsubscribe();
             };
         }
-    }, [userId, activeFolder]);
-
-    useEffect(() => {
-        console.log('activeFolder: ', activeFolder)
-        console.log('likedFolder: ', likedFolder)
-    }, [likedFolder])
+    }, [userId, activeFolder, isFocused]);
 
     return (
         <View
@@ -52,9 +52,23 @@ const ActiveCollectionScreen = () => {
                 paddingTop: insets.top,
                 paddingBottom: bottomTabBar + insets.bottom
             }}
-            className='bg-primary flex-grow'>
+            className='bg-primary flex-grow px-4'>
 
-            <Text>Your Active Folder</Text>
+            <Text className='text-center text-3xl font-bold mt-8 mb-8'>Your Active Folder</Text>
+
+            {likedFolder ?
+                <View className='bg-primary-300 rounded-2xl p-2 border border-accent h-40 flex flex-col justify-between'>
+                    <Text className='invisible text-primary-300'>padding</Text>
+                    <Text className='text-accent text-center text-medium text-2xl mb-6 justify-self-center'>{likedFolder?.name}</Text>
+                    <Text className='text-accent'>{likedFolder.items} items</Text>
+                </View> :
+                <Text className='text-center text-3xl font-bold mt-8 mb-8'>No Active Folder</Text>
+            }
+
+            <View>
+                <Text className='text-center mt-8'>This folder is in the <Text className='text-accent'>{activeFolder?.folderCategory}</Text> collection
+                </Text>
+            </View>
         </View>
     )
 }
