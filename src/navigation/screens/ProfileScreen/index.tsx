3a577c6fce4017c19ca7828d1520f1bdaf823f27
Cypher Moon <temperaturecaleb@gmail.com>
2024-04-11 19:08:05 +0100

    Add links to live website on the profile screen

--- src/navigation/screens/ProfileScreen/index.tsx
+++ src/navigation/screens/ProfileScreen/index.tsx
@@ -2,7 +2,7 @@ import ProfileItem from '@components/Profile/ProfileItem'
 import Text from '@components/common/Text'
 import { Image } from 'expo-image'
 import React from 'react'
-import { Platform, ScrollView, TouchableOpacity, View } from 'react-native'
+import { Linking, Platform, ScrollView, TouchableOpacity, View } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import { useAuthStore } from 'store/authStore'
 import { Entypo } from '@expo/vector-icons';
@@ -26,6 +26,7 @@ const ProfileScreen = () => {
     const _userImageUrl = useAuthStore(state => state.user?.image)
 
     const [userImage, setUserImage] = React.useState<CloudinaryResponse | undefined>(_userImageUrl)
+    const BASE_URL = 'https://memoria-landing-page.vercel.app'
 
     async function handleProfileChange() {
         // Launch the image picker
@@ -69,55 +70,54 @@ const ProfileScreen = () => {
                 paddingTop: insets.top,
                 paddingBottom: insets.bottom,
             }}>
-           
-                <View className='flex-row items-center mb-5 mt-16 rounded-lg px-3 py-1.5'>
 
-                    <TouchableOpacity onPress={handleProfileChange}>
-                        {userImage ? (
-                            <Image
-                                source={{ uri: userImage.secure_url }}
-                                className='w-16 h-16 rounded-full mr-3 border-gray-400 border-2'
-                            />
-                        ) : (
-                            <View className='relative w-16 h-16 rounded-full mr-3 border-gray-400 border-2 bg-primary-300 items-center justify-center p-1'>
-                                <Text className='text-[10px] text-center text-gray-400'>Update profile here</Text>
-                                <View className='absolute top-0 right-0 rounded-full'>
-                                    <Entypo name="edit" size={18} color={colors.gray['400']} />
-                                </View>
+            <View className='flex-row items-center mb-5 mt-16 rounded-lg px-3 py-1.5'>
+
+                <TouchableOpacity onPress={handleProfileChange}>
+                    {userImage ? (
+                        <Image
+                            source={{ uri: userImage.secure_url }}
+                            className='w-16 h-16 rounded-full mr-3 border-gray-400 border-2'
+                        />
+                    ) : (
+                        <View className='relative w-16 h-16 rounded-full mr-3 border-gray-400 border-2 bg-primary-300 items-center justify-center p-1'>
+                            <Text className='text-[10px] text-center text-gray-400'>Update profile here</Text>
+                            <View className='absolute top-0 right-0 rounded-full'>
+                                <Entypo name="edit" size={18} color={colors.gray['400']} />
                             </View>
-                        )}
-                    </TouchableOpacity>
-                    <Text className='text-xl font-semibold text-gray-300'>{userName}</Text>
-                </View>
-
-                {/*Action Section */}
-                <View className='mb-8'>
-                    <Text className='uppercase font-semibold text-xs text-gray-500 ml-3 mb-1.5'>Action</Text>
-                    <ProfileItem onPress={() => { }} text='Share with Friends' />
-                    {Platform.OS === 'ios' && <ProfileItem onPress={() => { }} text='Wallpaper Automation' />}
-
-                </View>
-
-                {/*Legal Section */}
-                <View className='mb-8'>
-                    <Text className='uppercase font-semibold text-xs text-gray-500 ml-3 mb-1.5'>Legal</Text>
-                    <ProfileItem onPress={() => { }} text='Terms of Service' />
-                    <ProfileItem onPress={() => { }} text='Privacy Policy' />
-                    <ProfileItem onPress={() => { }} text='AI Image Copyright' />
-                </View>
-
-                {/*Account Section */}
-                <ProfileItem
-                    onPress={() => handleSignOut()}
-                    showIcon={false}
-                    textCenter
-                    text='Logout'
-                />
-
-                <TouchableOpacity className='' onPress={() => { }}>
-                    <Text className='text-red-500 text-center'>Delete Account</Text>
+                        </View>
+                    )}
                 </TouchableOpacity>
-        
+                <Text className='text-xl font-semibold text-gray-300'>{userName}</Text>
+            </View>
+
+            {/*Action Section */}
+            <View className='mb-8'>
+                <Text className='uppercase font-semibold text-xs text-gray-500 ml-3 mb-1.5'>Action</Text>
+                <ProfileItem onPress={() => { }} text='Share with Friends' />
+                {Platform.OS === 'ios' && <ProfileItem onPress={() => { }} text='Wallpaper Automation' />}
+
+            </View>
+
+            {/*Legal Section */}
+            <View className='mb-8'>
+                <Text className='uppercase font-semibold text-xs text-gray-500 ml-3 mb-1.5'>Legal</Text>
+                <ProfileItem onPress={() => { Linking.openURL(`${BASE_URL}/privacy`) }} text='Privacy Policy' />
+                <ProfileItem onPress={() => { Linking.openURL(`${BASE_URL}/image-copyright`) }} text='AI Image Copyright' />
+            </View>
+
+            {/*Account Section */}
+            <ProfileItem
+                onPress={() => handleSignOut()}
+                showIcon={false}
+                textCenter
+                text='Logout'
+            />
+
+            <TouchableOpacity className='' onPress={() => { }}>
+                <Text className='text-red-500 text-center'>Delete Account</Text>
+            </TouchableOpacity>
+
         </View>
     )
 }
